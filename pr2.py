c9bbe863f33a19e653bac01547e87536efe9b605
unuqueness <114171492+unuqueness@users.noreply.github.com>
2022-09-25 16:17:50 +0300

    Delete pr2.py

--- pr2.py
+++
@@ -1,6 +0,0 @@
-import math
-x=float(input ("x="))
-y=float(input ("y="))
-z=float(input ("z="))
-s=((math.fabs(math.cos(x)-math.cos(y)))**(1+2*(math.sin(y)**2)))*(1+z+(z**2)/2+(z**3)/3+(z**4)/4)
-print("s= {0:.5f}".format (s))
